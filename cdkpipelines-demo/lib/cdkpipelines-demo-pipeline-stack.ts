38531e9f2fef438b4f21f09287c5b82058e306e4
Vladyslav <vk@asseco.dk>
2022-03-15 17:30:52 +0100

    Add PreProd stage

--- cdkpipelines-demo/lib/cdkpipelines-demo-pipeline-stack.ts
+++ cdkpipelines-demo/lib/cdkpipelines-demo-pipeline-stack.ts
@@ -1,6 +1,6 @@
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
-
+import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
 /**
  * The stack that defines the application pipeline
  */
@@ -30,7 +30,8 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
             }),
         });
 
-        // This is where we add the application stages
-        // ...
+        pipeline.addStage(new CdkpipelinesDemoStage(this, 'PreProd', {
+            env: { account: '061500401071', region: 'eu-central-1' }
+        }));
     }
 }
